7f82c7b4f329bab0ffbe10dd5dc2ac837512d84f
StepnexWorld <Stepnex1995@gmail.com>
2026-07-08 01:50:50 -0700

    Add mouse glow effect on cursor movement

--- script.js
+++ script.js
@@ -82,3 +82,13 @@ setTimeout(type,300);
 }
 
 type();
+/* Mouse Glow */
+
+const glow=document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left=e.clientX+"px";
+glow.style.top=e.clientY+"px";
+
+});
