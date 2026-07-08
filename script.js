d69dff623106f60f9e33ea1d58550fad18ab8eaf
StepnexWorld <Stepnex1995@gmail.com>
2026-07-08 01:36:56 -0700

    Add typing effect for words display
    Implement typing effect for dynamic text display.

--- script.js
+++ script.js
@@ -23,3 +23,62 @@ btn.style.transform="scale(1)";
 });
 
 });
+// Typing Effect
+
+const words = [
+"Shopify Developer",
+"Shopify Expert",
+"Ecommerce Specialist",
+"Frontend Developer"
+];
+
+let wordIndex = 0;
+let letterIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function type(){
+
+if(letterIndex < words[wordIndex].length){
+
+typing.innerHTML += words[wordIndex].charAt(letterIndex);
+
+letterIndex++;
+
+setTimeout(type,100);
+
+}else{
+
+setTimeout(erase,1800);
+
+}
+
+}
+
+function erase(){
+
+if(letterIndex>0){
+
+typing.innerHTML=words[wordIndex].substring(0,letterIndex-1);
+
+letterIndex--;
+
+setTimeout(erase,50);
+
+}else{
+
+wordIndex++;
+
+if(wordIndex>=words.length){
+
+wordIndex=0;
+
+}
+
+setTimeout(type,300);
+
+}
+
+}
+
+type();
